0e3e0220d6ea88a2c2db1f22dccd7a38b64ed4e3
small-rain <455062086@qq.com>
2019-02-23 23:04:55 +0800

    add KNN、LineRegression、Ridge、Lasso、ElasticNet model

--- projects/mobile_recommend/__init__.py
+++ projects/mobile_recommend/__init__.py
@@ -1,2 +1,5 @@
-import pandas as pd
-pd.to_datetime()
+import mglearn
+mglearn.plots.plot_ridge_n_samples()
+from sklearn.model_selection import learning_curve, KFold
+from sklearn.linear_model import ElasticNet
+en_model = ElasticNet()
